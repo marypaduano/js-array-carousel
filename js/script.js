3f6a9a1ce4c8f4e2c67705ade379774e4ba45737
marypaduano <maria.paduano@gmail.com>
2023-02-06 16:33:13 +0100

    slide added

--- js/script.js
+++ js/script.js
@@ -6,3 +6,33 @@ const rightArrow = document.querySelector('.arrow-right')
 const leftArrow = document.querySelector('.arrow-left')
 
 console.log(leftArrow,rightArrow)
+let activeSlide = 0
+
+rightArrow.addEventListener('click', function () {
+	console.log('current slide', activeSlide)
+
+	let currentSlide = imgElements[activeSlide]
+	currentSlide.classList.remove('active')
+
+	activeSlide++
+
+	let nextSlide = imgElements[activeSlide]
+	nextSlide.classList.add('active')
+    if (activeSlide>activeSlide.lenght)
+    console.log(activeSlide)
+}
+)
+	console.log('next slide', activeSlide)
+
+leftArrow.addEventListener('click', function () {
+    console.log('current slide', activeSlide)
+    
+    let currentSlide = imgElements[activeSlide]
+    currentSlide.classList.remove('active')
+    
+    activeSlide--
+    
+    let prevSlide = imgElements[activeSlide]
+    prevSlide.classList.add('active')
+}
+)
