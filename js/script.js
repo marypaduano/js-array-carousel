feb5780a6af98b1589954b3fa3c4649e353f9278
marypaduano <maria.paduano@gmail.com>
2023-02-06 15:48:38 +0100

    img added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log('carosello')
+
+const imgElements = document.getElementsByClassName('slide')
+console.log(imgElements)
+const rightArrow = document.querySelector('.arrow-right')
+const leftArrow = document.querySelector('.arrow-left')
+
+console.log(leftArrow,rightArrow)
